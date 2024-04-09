b7926681f617a421f2b82a8c2bc87815d5a5ac44
Kazuki Ochiai <kazu910739@gmail.com>
2024-04-09 23:10:59 +0900

    useEffect練習 part2

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,18 +1,45 @@
 import { useEffect, useState } from 'react'
 
 const Timer = () => {
+    const [isDisp, setIsDisp] = useState(true);
+    return (
+        <>
+            <h1>UseEffect練習</h1>
+            <div>
+                {isDisp && <Timer2 />}
+            </div>
+            <button onClick={() => setIsDisp(prev => !prev)}>
+                トグル
+            </button >
+        </>
+    )
+}
+const Timer2 = () => {
     const [time, setTime] = useState(0);
 
     // コンポーネントが生成されたときに一回だけ呼ぶ
     // -> useEffectの第二引数に空の配列を渡す
     useEffect(() => {
-        window.setInterval(() => {
+        let intervalId = null;
+        intervalId = window.setInterval(() => {
+            console.log('interval called');
             setTime(prev => prev + 1);
         }, 1000);
+        // コンポーネントが破棄されるときに呼ぶ
+        return () => {
+            window.clearInterval(intervalId);
+        };
     }, [])
+
+    // useEffect(() => {
+    //     document.title = 'counter: ' + time;
+    //     window.localStorage.setItem('counter', time.toString());
+    //     // 依存配列に設定した変数をuseEffect内で更新すると無限ループになるため注意
+    //     // setTime(prev => prev + 1);
+    // }, [time])
+
     return (
         <>
-            <h1>UseEffect練習</h1>
             <time>{time}</time>
             <span>秒経過</span>
 
